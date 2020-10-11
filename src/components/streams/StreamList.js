0744832a45a58720d051fd563f53e254f7ef5b4f
Lauri Kemppainen <laukempp@gmail.com>
2020-10-11 23:54:20 +0300

    StreamList component now displays all streams

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions/index";
 
-const StreamList = () => {
-  return <div>StreamList</div>;
+const StreamList = ({ fetchStreams, streams }) => {
+  useEffect(() => {
+    fetchStreams();
+  }, []);
+
+  console.log(streams);
+
+  const renderList = () => {
+    return streams.map((stream) => {
+      return (
+        <div className="item" key={stream.id}>
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <div>
+      <h2>Streams</h2>
+      <div className="ui celled list">{renderList()}</div>
+    </div>
+  );
 };
 
-export default StreamList;
+const mapStateToProps = (state) => {
+  return {
+    streams: Object.values(state.streams),
+  };
+};
+
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
