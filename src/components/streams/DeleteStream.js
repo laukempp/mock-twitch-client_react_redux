03b75121351576601158e26fe1ca92b8f2cd03f9
Lauri Kemppainen <laukempp@gmail.com>
2020-10-12 17:20:21 +0300

    created an initial modal for DeleteStream component using portals

--- src/components/streams/DeleteStream.js
+++ src/components/streams/DeleteStream.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Modal from "../modal/Modal";
 
 const DeleteStream = () => {
-  return <div>DeleteStream</div>;
+  return (
+    <div>
+      DeleteStream
+      <Modal />
+    </div>
+  );
 };
 
 export default DeleteStream;
